dbdcf023c7ed125563fa55203788533b8b2ab8f2
Ferencz Istvan <dorottya.retyi@teleporthq.io>
2024-04-08 17:42:09 +0300

    some modification and added console logs

--- hazi-fi-JS.js
+++ hazi-fi-JS.js
@@ -1,18 +1,23 @@
 let a = 6;
 let b = 5;
 
-osszeg = (a, b) => {
+amount = (a, b) => {
     return a+b;
 };
 
-kulonbseg = (a, b) => {
+difference = (a, b) => {
     return a-b;
 }
 
-szorzat = (a, b) => {
+product = (a, b) => {
     return a*b;
 }
 
-hanyados = (a,b) => {
+ratio = (a,b) => {
     return a/b;
 }
+
+console.log(`A két szám összege: ${amount(a, b)}`);
+console.log(`A két szám különbsége: ${difference(a, b)}`);
+console.log(`A két szám szorzata: ${product(a, b)}`);
+console.log(`A két szám hányadosa: ${ratio(a, b)}`)
